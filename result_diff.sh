3f3b5f27c095825d70bc46caef5b9aacea92698c
Sebastian Thelen <sebastianthelen@gmail.com>
2014-06-18 09:22:22 +0200

    moved diff results into folder diff

--- result_diff.sh
+++ result_diff.sh
@@ -3,6 +3,6 @@
 
 for i in `seq 1 14`;
 do
-diff3 virtuoso/results/normalized_sparql$i.json allegrograph/results/normalized_sparql$i.json stardog/results/normalized_sparql$i.json > result_diff$i.txt
+diff3 virtuoso/results/normalized_sparql$i.json allegrograph/results/normalized_sparql$i.json stardog/results/normalized_sparql$i.json > diff/result_diff$i.txt
 done 
 
