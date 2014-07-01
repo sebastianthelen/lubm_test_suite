f334abc37306893eda05133ca683937f13149485
unknown <thelese@OPPC419.publications.win>
2014-07-01 09:59:39 +0200

    set inference level to EL

--- stardog/run_test_suite_lubm1000.sh
+++ stardog/run_test_suite_lubm1000.sh
@@ -39,7 +39,7 @@ do
 	for i in 0 1 2 3 4 5 6 7 8 9 10 11
 	do
 	printf "%d,%d," "$j" "$i" >> log.csv
-	t=$((/usr/bin/time -f'%e' curl -G --silent --max-time '300' --header "Accept: application/sparql-results+xml" --header "X-Requested-With:XMLHttpRequest" --header "Authorization:Basic YWRtaW46YWRtaW4=" --header "SD-Connection-String:reasoning=NONE" 'http://abel:5820/annex/lubm1000DB/sparql/query' --data-urlencode 'query='"${query[$i]}"'' > results/lubm1000/sparql$i-$j.xml) 2>&1)
+	t=$((/usr/bin/time -f'%e' curl -G --silent --max-time '300' --header "Accept: application/sparql-results+xml" --header "X-Requested-With:XMLHttpRequest" --header "Authorization:Basic YWRtaW46YWRtaW4=" --header "SD-Connection-String:reasoning=EL" 'http://abel:5820/annex/lubm1000DB/sparql/query' --data-urlencode 'query='"${query[$i]}"'' > results/lubm1000/sparql$i-$j.xml) 2>&1)
 	printf "%s," "$t" >> results/lubm1000/log.csv
 	xsltproc ../extract_bindings.xslt results/lubm1000/sparql$i-$j.xml >> results/lubm1000/log.csv;
 	printf "\n" >> results/lubm1000/log.csv
